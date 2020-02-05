b1dd39175d19002ba64b29b7ec3d2c73d1cd1b5b
galih indra rukmana <galih.rukmana@wgs.co.id>
2020-02-05 16:34:38 +0700

    remove usb 2002051634

--- views/layouts/content.php
+++ views/layouts/content.php
@@ -14,8 +14,5 @@ use dmstr\widgets\Alert;
 </div>
 
 <footer class="main-footer" style="font-size:11px">
-    <div class="pull-right hidden-xs">
-        <b>Developed By Internofa IT Solution</b>
-    </div>
-     <strong>&copy; Sistem Informasi Akademik Universitas Sangga Buana YPKP <?= date("Y")?> | Design By <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong>
+
 </footer>
