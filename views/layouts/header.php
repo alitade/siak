d71cff6361b5617ba5024fd4d5cc50bb36a917b1
galih indra rukmana <galih.rukmana@wgs.co.id>
2020-02-05 16:47:29 +0700

    fixing main menu

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -19,7 +19,7 @@ switch($usr->tipe){
 <header class="main-header">
     <?= Html::a('<span class="logo-mini">SIA</span><span class="logo-lg" style="font-size:16px">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
     <nav class="navbar navbar-static-top" role="navigation">
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
         <div class="navbar-custom-menu">
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,51 +1,16 @@
 <?php
-
 use yii\helpers\Html;
-use yii\helpers\Url;
-use app\models\Funct;
-/* @var $this \yii\web\View */
-/* @var $content string */
+$F = (new \app\models\Funct);
 
 
-use yii\web\AssetBundle;
-class AdminLtePluginAsset extends AssetBundle
-{
-    public $sourcePath = '@vendor/almasaeed2010/adminlte/plugins';
-    public $js = [
-        'datatables/dataTables.bootstrap.min.js',
-        // more plugin Js here
-    ];
-    public $css = [
-        'datatables/dataTables.bootstrap.css',
-        // more plugin CSS here
-    ];
-    public $depends = [
-        'dmstr\web\AdminLteAsset',
-    ];
-}
-
-$User = Yii::$app->user;
-#maintenance
-#return Yii::$app->response->redirect(Url::to("@web/../under/index.html"));
-if(isset($_GET['d1r1th3h3'])){
-    if($_GET['d1r1th3h3']==1){
-        $_SESSION['huha']=1;
-        return Yii::$app->response->redirect(Url::to(['site/index']));
-    }
-}
-
-if ( $User->isGuest or Yii::$app->controller->action->id === 'login') {
-    return Yii::$app->response->redirect(Url::to(['site/login']));
+//dd(Yii::$app->user->identity->ccode);
+if (Yii::$app->controller->action->id === 'login') {
+    echo $this->render('main-login',['content' => $content]);
 } else {
-
-	if(app\models\Funct::cekPassDef()){
-		if(Yii::$app->controller->action->id!=='change-password'){
-			return Yii::$app->response->redirect(Url::to(['site/change-password']));
-		}
-	}	
+    if(Yii::$app->user->isGuest){Yii::$app->response->redirect(['site/login']);}
 
     app\assets\AdminLteAsset::register($this);
-    app\assets\AdminLteBowerAsset::register($this);
+    //app\assets\AdminLteBowerAsset::register($this);
 
     $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
     ?>
@@ -58,21 +23,17 @@ if ( $User->isGuest or Yii::$app->controller->action->id === 'login') {
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
-    <link rel="shortcut icon" type="image/x-icon" href="<?=Yii::getAlias('@web').'/images/ypkp.png'?>">
     </head>
-    <body class="hold-transition skin-blue sidebar-mini">
+    <body class="skin-black fixed sidebar-mini sidebar-mini-expand-feature">
     <?php $this->beginBody() ?>
     <div class="wrapper">
+
         <?= $this->render(
             'header.php',
             ['directoryAsset' => $directoryAsset]
         ) ?>
 
-        <?= $this->render(
-            'left.php',
-            ['directoryAsset' => $directoryAsset,'F'=>new Funct()]
-        )
-        ?>
+        <?= $this->render('left.php',['directoryAsset' => $directoryAsset,'F'=>$F])?>
 
         <?= $this->render(
             'content.php',
